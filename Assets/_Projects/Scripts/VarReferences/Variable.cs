7620a769a25f5de0ca3f641c857f81c5a141ac78
MupetCol <mupet.col@gmail.com>
2024-06-05 20:37:31 -0500

    Added Reset Method to SO Variables
    - All SO variables have a generic BaseValue, if the editor bool of Reset is checked (checked by default) they'll reset themselves on enable

--- Assets/_Projects/Scripts/VarReferences/Variable.cs
+++ Assets/_Projects/Scripts/VarReferences/Variable.cs
@@ -6,10 +6,14 @@ namespace GPP
     //Variables and References, a great tool for abstraction and easy inspector design
     public class Variable<T> : ScriptableObject
     {
+        public bool Reset = true;
         public T Value;
+        public T BaseValue;
+
 
         private void OnEnable()
         {
+            if(Reset) Value = BaseValue;
             hideFlags = HideFlags.DontUnloadUnusedAsset;
         }
     }
